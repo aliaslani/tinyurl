a18b08c1c999da2dcb7f496d7b7583b23aeede84
aliaslani <cinortcele1@gmail.com>
2023-10-02 12:01:23 +0330

    add filter to urls

--- shortenner/views.py
+++ shortenner/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import action
 from .models import URL
 from django.contrib.auth.models import User
 from .utils import generate_short_url
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
@@ -46,8 +47,11 @@ class UserViewSet(viewsets.ModelViewSet):
 class URLViewSet(viewsets.ModelViewSet):
     queryset = URL.objects.select_related('user').all()
     serializer_class = URLSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user']
     throttle_classes = [UserRateThrottle]
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
     def perform_create(self, serializer):
         user = self.request.user
